5b89cf2397b6f980da3d38cb1cd3249f151771dd
hyeon-a12 <105984535+hyeon-a12@users.noreply.github.com>
2026-07-07 18:58:50 +0900

    expo 버전 수정 46 -> 45

--- src/screens/Camera/CameraScreen.tsx
+++ src/screens/Camera/CameraScreen.tsx
@@ -223,7 +223,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   recordingBorder: {
-    ...StyleSheet.absoluteFill,
+    ...StyleSheet.absoluteFillObject,
     borderWidth: 6,
     borderColor: COLORS.cameraRecordingBorder,
   },
