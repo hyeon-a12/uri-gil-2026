84ffa3139f3e0a9386981c3b99c8c69d0747d952
ge323 <122430397+ge323@users.noreply.github.com>
2026-08-31 21:10:40 +0900

    첫 화면 홈 말고 온보딩으로 가게 수정 하기

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from 'expo-router';
 
 export default function Index() {
   //return <Redirect href="/onboarding" />;
-  return <Redirect href="/home" />; //홈으로 바로 가게
+  return <Redirect href="/onboarding" />; //홈으로 바로 가게
 }
